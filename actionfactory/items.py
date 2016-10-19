73cba2545c9f50c9886498e99c2f0057fda1f9e0
sonya75 <sonyaroy75@gmail.com>
2016-10-19 18:58:42 +0530

    Update items.py

--- actionfactory/items.py
+++ actionfactory/items.py
@@ -1,11 +1,14 @@
 class Item:
 	def __init__(self,city,castleid=None):
-		global castleId
 		self.city=city
-		castleId=castleid
-	def useitem(self,itemid,castleid=castleId):
+		self.castleid=castleid
+	def useitem(self,itemid,castleid=None):
+		if castleid==None:
+			castleid=self.castleid
 		self.city.client.sendmessage('shop.useGoods',{'itemId':itemid,'castleId':castleid,'num':1})
 		res=self.city.responsehandler('shop.useGoods')
-	def buyitem(self,itemid,castleid=(self.castleid)):
+	def buyitem(self,itemid,castleid=None):
+		if castleid==None:
+			castleid=self.castleid
 		self.city.client.sendmessage('shop.buy',{'itemId':itemid,'amount':1})
 		res=self.city.responsehandler('shop.buy')
